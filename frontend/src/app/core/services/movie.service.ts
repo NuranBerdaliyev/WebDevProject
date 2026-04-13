cf9ecebc9a4eaa8e0d043a915f40a566c81a008c
Adi Dossabayev <adi_d@inbox.ru>
2026-04-13 23:13:54 +0500

    add movie service with basic API methods

---
+++ frontend/src/app/core/services/movie.service.ts
@@ -0,0 +1,22 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { Movie } from '../models/movie.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/movies`;
+
+  getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${this.apiUrl}/`);
+  }
+
+  getMovieById(id: number): Observable<Movie> {
+    return this.http.get<Movie>(`${this.apiUrl}/${id}/`);
+  }
+}
